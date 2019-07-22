a1d15dac20634fe8bea8aa6e8c14b4fa2144432f
御承扬 <2923616405@qq.com>
2019-07-22 22:24:29 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    if_test(18);
+    if_test(5);
+    if_else_test(20);
+    if_else_test(2);
+}
+
+fn if_test(x:i32){
+    if x > 10 {
+        println!("the passed argument is greater than 10, And its value is {}",x);
+    }
+}
+
+fn if_else_test(x:i32) {
+    if x > 10 {
+        println!("the passed argument is greater than 10, And its value is {}",x);
+    } else {
+        println!("The passed argument is less than the integer 10, the value of the argument is {}",x);
+    }
 }
